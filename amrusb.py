5eba21d50d19f36a81c57e365ac9ac091d4bd9d8
Peter Rukavina <peter@rukavina.net>
2014-09-10 17:59:53 -0300

    Added exception handling.
    The script was crashing and I didn’t know why (the Pi was losing its
    wifi connection, it turns out), so I added exception handling to trap
    issues like this.

--- amrusb.py
+++ amrusb.py
@@ -3,6 +3,8 @@
 import serial
 import logging
 import time
+import sys
+
 from time import strftime
 import boto
 from boto.dynamodb2.table import Table
@@ -11,6 +13,11 @@ class App():
 
     def __init__(self):
         self.device = '/dev/ttyACM0'
+        self.stdin_path = '/dev/null'
+        self.stdout_path = '/dev/tty'
+        self.stderr_path = '/dev/tty'
+        self.pidfile_path =  '/var/run/amrusb/amrusb.pid'
+        self.pidfile_timeout = 5
 
     def run(self):
         logger.info('Starting amrusb daemon')
@@ -19,15 +26,23 @@ class App():
         readings = Table('readings')
         ser = serial.Serial(self.device)
         while True:
-            line = ser.readline().rstrip()
-            logger.info(line)
-            amrvalues = line.split(',')
-            if (amrvalues[0] == '$UMSCM'):
-                readingvalue = amrvalues[3].split('*');
-                readings.put_item(data={'serialnumber': int(amrvalues[1]), 'datestamp': int(time.time()), 'reading': int(readingvalue[0]), 'type': int(amrvalues[2])})
+            try:
+                line = ser.readline().rstrip()
+            except:
+                logger.error(sys.exc_info()[0])
+                raise
+            else:
+                logger.info(line)
+                amrvalues = line.split(',')
+                if (amrvalues[0] == '$UMSCM'):
+                    readingvalue = amrvalues[3].split('*');
+                    try:
+                        readings.put_item(data={'serialnumber': int(amrvalues[1]), 'datestamp': int(time.time()), 'reading': int(readingvalue[0]), 'type': int(amrvalues[2])})
+                    except:
+                        logger.error(sys.exc_info()[0])
+                        raise
 
 app = App()
-
 logger = logging.getLogger('DaemonLog')
 logger.setLevel(logging.INFO)
 formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
