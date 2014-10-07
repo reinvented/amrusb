fdf5e171e1500612af74348c7003063d4075e67f
Peter Rukavina <peter@rukavina.net>
2014-10-07 16:38:41 -0600

    Simplify the "display only" script -- no logging.

--- displayonly.py
+++ displayonly.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 import serial
-import logging
 import time
 import sys
 
@@ -11,11 +10,6 @@ class App():
 
     def __init__(self):
         self.device = '/dev/ttyACM0'
-        self.stdin_path = '/dev/null'
-        self.stdout_path = '/dev/tty'
-        self.stderr_path = '/dev/tty'
-        self.pidfile_path =  '/var/run/amrusb/amrusb.pid'
-        self.pidfile_timeout = 5
 
     def run(self):
         logger.info('Starting amrusb daemon')
@@ -25,17 +19,10 @@ class App():
             try:
                 line = ser.readline().rstrip()
             except:
-                logger.error('Could not read from the AMRUSB-1');
-                logger.error(sys.exc_info()[0])
+                print 'Could not read from the AMRUSB-1'
+                print sys.exc_info()[0]
             else:
-                logger.info(line)
+                print line
 
 app = App()
-logger = logging.getLogger('DaemonLog')
-logger.setLevel(logging.INFO)
-formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-handler = logging.FileHandler('/var/log/amrusb/amrusb.log')
-handler.setFormatter(formatter)
-logger.addHandler(handler)
-
 app.run()
